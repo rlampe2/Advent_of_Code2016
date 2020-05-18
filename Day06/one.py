434db9ae7dccfa4dae97bf7fbe5b6893abc24ed6
Ryan Lampe <rjlampe12@gmail.com>
2020-05-18 12:34:35 -0500

    part one done!

---
+++ Day06/one.py
@@ -0,0 +1,27 @@
+# Author: Ryan Lampe
+# 5/18/2020
+# Advent of Code 2016
+# Day 6, Problem 1
+
+from collections import Counter
+# Have a list of lists for each index, and add all the characters in that column to it.
+# Then for each list, print the most common character
+
+index = {}
+
+
+with open('input.txt', 'r') as file:
+    lines = file.read().splitlines() # Use instead of readlines to get rid of newlines
+
+for i in range(0, len(lines[0])):
+    index[i] = []  # create a list at that index
+
+for line in lines:
+    for i in range(0, len(line)):
+        index[i].append(line[i])
+
+# Find most common char:
+for i in range(0, len(lines[0])):
+    c = Counter(index[i]).most_common(1)
+    for key, val in c:
+        print(key)  # There's only one entry in c, but want just the key
